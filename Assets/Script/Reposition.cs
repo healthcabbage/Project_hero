1306288109f95f3ee7ed122b8a66ad8cece6c346
Cabbage <49223403+healthcabbage@users.noreply.github.com>
2023-06-29 15:07:52 +0900

    무한 맵
    - 플레이어가 이동할때마다 따라가는 무한 맵 생성
    - 플레이어를 따라다니는 카메라 생성

---
+++ Assets/Script/Reposition.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reposition : MonoBehaviour
+{
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Area"))
+        {
+            return;
+        }
+
+        Vector3 playerPos = GameManager.instance.player.transform.position;
+        Vector3 myPos = transform.position;
+        float dirX = playerPos.x - myPos.x;
+        float dirY = playerPos.y - myPos.y;     
+        float diffx = Mathf.Abs(dirX);
+        float diffy = Mathf.Abs(dirY);
+
+        Vector3 playerDir = GameManager.instance.player.inputVec;
+        dirX = dirX > 0 ? 1 : -1;
+        dirY = dirY > 0 ? 1 : -1;
+
+        switch (transform.tag)
+        {
+            case "Ground":
+                if (diffx > diffy)
+                {
+                    transform.Translate(Vector3.right * dirX * 40);
+                }
+                else if (diffx < diffy)
+                {
+                    transform.Translate(Vector3.up * dirY * 40);
+                }
+                break;
+            case "Enemy":
+
+                break;
+        }
+
+    }
+}
