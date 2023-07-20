de6362c1b68f51ee27f64e1008c6460a0f8a160c
Cabbage <49223403+healthcabbage@users.noreply.github.com>
2023-07-20 15:42:37 +0900

    레벨업 1차 작업
    - 랜덤으로 레벨업 생성
    - UI 이미지가 제대로 맞지않아 수정 작업

---
+++ Assets/Script/LevelUp.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelUp : MonoBehaviour
+{
+    RectTransform rect;
+    Item[] items;
+
+    private void Awake()
+    {
+        rect = GetComponent<RectTransform>();
+        items = GetComponentsInChildren<Item>(true);
+    }
+
+    public void Show()
+    {
+        Next();
+        rect.localScale = Vector3.one;
+        GameManager.instance.Stop();
+    }
+
+    public void Hide()
+    {
+        rect.localScale = Vector3.zero;
+        GameManager.instance.Resume();
+    }
+
+    public void Select(int index)
+    {
+        items[index].OnClick();
+    }
+
+    void Next()
+    {
+        // 1. 모든 아이템 비활성화
+        foreach(Item item in items)
+        {
+            item.gameObject.SetActive(false);
+        }
+        // 2. 그 중에서 랜덤하게 3개 아이템 활성화
+        int[] random = new int[3];
+        while (true)
+        {
+            random[0] = Random.Range(0, items.Length);
+            random[1] = Random.Range(0, items.Length);
+            random[2] = Random.Range(0, items.Length);
+
+            if (random[0] != random[1] && random[1] != random[2] && random[0] != random[2])
+                break;
+        }
+
+        for (int index = 0; index < random.Length; index++)
+        {
+            Item ranItem = items[random[index]];
+
+            // 3. 만렙 아이템의 경우는 소비아이템으로 대체
+            if (ranItem.level == ranItem.data.damages.Length)
+            {
+                items[4].gameObject.SetActive(true);
+            }
+            else
+            {
+                ranItem.gameObject.SetActive(true);
+            }
+
+        }
+    }
+}
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -24,18 +24,27 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        if (!GameManager.instance.isLive)
+            return;
+
         inputVec.x = Input.GetAxisRaw("Horizontal");
         inputVec.y = Input.GetAxisRaw("Vertical");
     }
 
     void FixedUpdate()
     {
+        if (!GameManager.instance.isLive)
+            return;
+
         Vector2 nextVector = inputVec.normalized * speed * Time.fixedDeltaTime;
         rigid.MovePosition(rigid.position + nextVector);
     }
 
     void LateUpdate()
     {
+        if (!GameManager.instance.isLive)
+            return;
+
         anim.SetFloat("Speed", inputVec.magnitude);
 
         if (inputVec.x != 0)
